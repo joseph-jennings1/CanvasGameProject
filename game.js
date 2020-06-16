5535e4560a1bd16b5ebc57bbb92f14b0855c9e80
joseph-jennings1 <40963397+joseph-jennings1@users.noreply.github.com>
2020-06-15 20:42:13 -0700

    Update game.js

--- game.js
+++ game.js
@@ -29,12 +29,43 @@ monsterImage.onload = function () {
 };
 monsterImage.src = "images/monster.png";
 
+// Mage image
+var mageReady = false;
+var mageImage = new Image();
+mageImage.onload = function () {
+	mageReady = true;
+};
+mageImage.src = "images/Mage.png";
+
+// Thug image
+var thugReady = false;
+var thugImage = new Image();
+thugImage.onload = function () {
+	thugReady = true;
+};
+thugImage.src = "images/Thug.png";
+
+// Spearman image
+var spearmanReady = false;
+var spearmanImage = new Image();
+spearmanImage.onload = function () {
+	spearmanReady = true;
+};
+spearmanImage.src = "images/Spearman.png";
+
 // Game objects
 var hero = {
-	speed: 256 // movement in pixels per second
+	speed: 5 // movement in pixels per second
 };
 var monster = {};
+var mage = {};
+var thug = {};
+var spearman = {};
 var monstersCaught = 0;
+var totalCaught = 0;
+var magesCaught = 0;
+var spearmenCaught = 0;
+var thugsCaught = 0;
 var monstersLeft = 10;
 var currentScore = 0;
 var highScore = 0;
@@ -57,38 +88,52 @@ var reset = function () {
 	hero.y = canvas.height / 2;
 
 	// Throw the monster somewhere on the screen randomly
-	monster.x = 32 + (Math.random() * (canvas.width - 64));
-	monster.y = 32 + (Math.random() * (canvas.height - 64));
+	resetMonster();
 };
+
 
 //change this to 'clearMonster' after adding more monster sprites for gameplay
 var resetMonster = function() {
 	// Throw the monster somewhere on the screen randomly
-	monster.x = 32 + (Math.random() * (canvas.width - 64));
-	monster.y = 32 + (Math.random() * (canvas.height - 64));	
+	monster.x = (Math.random() * (canvas.width - 64));
+	monster.y = (Math.random() * (canvas.height - 64));	
+	if (monster.x < 45 || monster.x > 559 || monster.y < 38 || monster.y > 559) resetMonster();
+	
+	mage.x = (Math.random() * (canvas.width - 64));
+	mage.y = (Math.random() * (canvas.height - 64));	
+	if (mage.x < 45 || mage.x > 559 || mage.y < 38 || mage.y > 559) resetMonster();
+	
+	thug.x = (Math.random() * (canvas.width - 64));
+	thug.y = (Math.random() * (canvas.height - 64));	
+	if (thug.x < 45 || thug.x > 559 || thug.y < 38 || thug.y > 559) resetMonster();
+	
+	spearman.x = (Math.random() * (canvas.width - 64));
+	spearman.y = (Math.random() * (canvas.height - 64));	
+	if (spearman.x < 45 || spearman.x > 559 || spearman.y < 38 || spearman.y > 559) resetMonster();
 }
 
 // Update game objects
 var update = function (modifier) {
-	if (38 in keysDown) { // Player holding up
-		if (hero.y - (hero.speed * modifier) > 0) {
-			hero.y -= hero.speed * modifier;
-			edgeDetection();
-			if (collision == 1) {
-				hero.y += hero.speed * modifier;
-				collision = 0;
-			}
-		}
-		
-	}
+
+	//basic movement
+	
+	if (38 in keysDown) { // Player holding down
+		if (hero.y - (hero.speed) > 38) {
+			hero.y -= hero.speed;
+		} 
+	}	
 	if (40 in keysDown) { // Player holding down
-		hero.y += hero.speed * modifier;
+		if(hero.y + (hero.speed) < 559){
+			hero.y += hero.speed;
+		}
 	}
 	if (37 in keysDown) { // Player holding left
-		hero.x -= hero.speed * modifier;
+		if(hero.x + (hero.speed) > 45)
+		hero.x -= hero.speed;
 	}
 	if (39 in keysDown) { // Player holding right
-		hero.x += hero.speed * modifier;
+		if(hero.x + (hero.speed) < 559)
+		hero.x += hero.speed;
 	}
 
 	// Are they touching?
@@ -98,32 +143,58 @@ var update = function (modifier) {
 		&& hero.y <= (monster.y + 32)
 		&& monster.y <= (hero.y + 32)
 	) {
-		++monstersCaught;
-		currentScore = monstersCaught*10;
+		monstersLeft--;
+		currentScore = currentScore +10;
 		if (currentScore > highScore) {
 			highScore = currentScore;
 		}
-		--monstersLeft;
-		if (monstersLeft == 0) {
-			//resetGame
-			//monstersLeft = 10;
-			//currentScore = 0;
+		//change to a 'clearMonster' function after adding more monsters
+		resetMonster();
+	}
+	if (
+		hero.x <= (mage.x + 32)
+		&& mage.x <= (hero.x + 32)
+		&& hero.y <= (mage.y + 32)
+		&& mage.y <= (hero.y + 32)
+	) {
+		monstersLeft--;
+		currentScore = currentScore +20;
+		if (currentScore > highScore) {
+			highScore = currentScore;
+		}
+		//change to a 'clearMonster' function after adding more monsters
+		resetMonster();
+	}
+	if (
+		hero.x <= (thug.x + 32)
+		&& thug.x <= (hero.x + 32)
+		&& hero.y <= (thug.y + 32)
+		&& thug.y <= (hero.y + 32)
+	) {
+		monstersLeft--;
+		currentScore = currentScore +30;
+		if (currentScore > highScore) {
+			highScore = currentScore;
+		}
+		//change to a 'clearMonster' function after adding more monsters
+		resetMonster();
+	}
+	if (
+		hero.x <= (spearman.x + 32)
+		&& spearman.x <= (hero.x + 32)
+		&& hero.y <= (spearman.y + 32)
+		&& spearman.y <= (hero.y + 32)
+	) {
+		monstersLeft--;
+		currentScore = currentScore +40;
+		if (currentScore > highScore) {
+			highScore = currentScore;
 		}
 		//change to a 'clearMonster' function after adding more monsters
 		resetMonster();
 	}
 };
 
-//Edge detection
-var edgeDetection = function () {
-	var checkedImage = ctx.getImageData(x, y, 15, 15);
-	var checkedPixel = checkedImage.data;
-	for (var i = 0; j = checkedPixel.length, i < j; i += 4) {
-		if (checkedPixel[i] == 51) {
-			collision = 1;
-		}
-	}
-};
 
 // Draw everything
 var render = function () {
@@ -137,6 +208,15 @@ var render = function () {
 
 	if (monsterReady) {
 		ctx.drawImage(monsterImage, monster.x, monster.y);
+	}
+	if (mageReady) {
+		ctx.drawImage(mageImage, mage.x, mage.y);
+	}
+	if (thugReady) {
+		ctx.drawImage(thugImage, thug.x, thug.y);
+	}
+	if (spearmanReady) {
+		ctx.drawImage(spearmanImage, spearman.x, spearman.y);
 	}
 	
 	// Current Score
@@ -159,7 +239,32 @@ var render = function () {
 	ctx.textAlign = "left";
 	ctx.textBaseline = "top";
 	ctx.fillText("High score: " + highScore, 435, 600);
+
+	if (monstersLeft <= 0) {
+		if (currentScore < highScore) {
+			if(confirm("You have caught ten monsters and your score is " + currentScore + ". The high score is " +highScore +". Press \"ok\" to play again.")){
+				monstersLeft = 10;
+				currentScore = 0;
+				monstersCaught = 0;
+				reset();
+				main();
+			}
+			else txt = "Goodbye."
+		} 
+		else {
+			if(confirm("Congratulations! You broke or tied the high score with " +currentScore + " points! Press \"ok\" to play again.")){
+				monstersLeft = 10;
+				currentScore = 0;
+				monstersCaught = 0;
+				reset();
+				main();
+			}
+			else txt = "Goodbye."
+		}
+	}
 };
+
+
 
 // The main game loop
 var main = function () {
